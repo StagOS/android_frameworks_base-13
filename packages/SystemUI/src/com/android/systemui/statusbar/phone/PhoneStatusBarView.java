ed18a326f71fd86f29aa34d2ae69fb800a111ebf
Lars Greiss <kufikugel@googlemail.com>
2012-12-17 05:11:21 -0600

    SystemUI: statusbar clock date customization [1/2]
    This commit add support for:
    
    - Clock enable/disable
    - Clock position
    - Clock seconds
    - AM/PM styles multilang support
    - Date format
    - Date style
    
    @mydongistiny:
    -Ported to Pie
    -Updated Left/Right Clock
    -Fixed heads-up enabling left clock when disabled
    
    @spezi77:
    - Updated for Android10
    - Adapted for ContentObserver
    - Add back clock seconds (why not though?)
    - Add clock center/right position
    - Add date right/left position
    - Fixed "Clock is double after reboot or update"
    - Clock: Fix crash in formatted string with date on right
    - CollapsedStatusbar: Fix phantom spaces in status bar
    - Make date/clock view play nice with collapsed/expanded statusbar
    - Fixed "system icons appear twice on lockscreen"
    - Fixed "Clock appears despite that it has been set to hidden"
    - Play nice with collapsed/expanded statusbar
    - Play nice with heads-up notifications
    - Make settings strings consistent: "status_bar" -> "statusbar"
    
    AEX Edits:
    - Fix statusbar clock color on light wallpapers and theme (Android 12)
    
    SuperDroidBond edits:
    - minor edits for A13
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
    Change-Id: I4690e221318a9c7bdc1c32c9b7eceda16825289e
    
    -----
    
    SystemUI: Make sure notification icons are sticked to the left
    
    Sometimes they are not when using center clock
    
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
    Change-Id: I435c84787eeef29d419fd5afcd0cb8379b573226
    
    -----
    
    base: Allow additional padding for center clock
    
    * When the punch hole overlay is filled in, the center clock
      position will be calculated ignoring the punch hole, which
      will cause center clock too left or right.
    
      Allow additional padding on center clock to make it also
      center on punch hole devices.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Ide45f637fc53fda7eb2b037c3254260576f10dd3
    
    -----
    
    base: Make center clock follow paddingTop
    
    * @neobuddy89: Apply paddingTop to center clock layout instead clockview itself.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    -----
    
    SystemUI: Make center clock layout safe insets aware
    
    Change-Id: I98ea985eacbcfce0b52322ce1df3d7c9ec564a3d
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Signed-off-by: Hyper <hyper@remainsilent.net>
    Signed-off-by: Cruzer1 <preetamswain143@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
@@ -26,6 +26,7 @@ import android.util.Log;
 import android.util.Pair;
 import android.view.DisplayCutout;
 import android.view.MotionEvent;
+import android.view.Surface;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowInsets;
@@ -51,6 +52,8 @@ public class PhoneStatusBarView extends FrameLayout {
 
     private DarkReceiver mBattery;
     private DarkReceiver mClock;
+    private DarkReceiver mClockCentre;
+    private DarkReceiver mClockRight;
     private int mRotationOrientation = -1;
     @Nullable
     private View mCutoutSpace;
@@ -86,6 +89,8 @@ public class PhoneStatusBarView extends FrameLayout {
         super.onFinishInflate();
         mBattery = findViewById(R.id.battery);
         mClock = findViewById(R.id.clock);
+        mClockCentre = findViewById(R.id.center_clock);
+        mClockRight = findViewById(R.id.right_clock);
         mCutoutSpace = findViewById(R.id.cutout_space_view);
 
         updateResources();
@@ -97,6 +102,8 @@ public class PhoneStatusBarView extends FrameLayout {
         // Always have Battery meters in the status bar observe the dark/light modes.
         Dependency.get(DarkIconDispatcher.class).addDarkReceiver(mBattery);
         Dependency.get(DarkIconDispatcher.class).addDarkReceiver(mClock);
+        Dependency.get(DarkIconDispatcher.class).addDarkReceiver(mClockCentre);
+        Dependency.get(DarkIconDispatcher.class).addDarkReceiver(mClockRight);
         if (updateDisplayParameters()) {
             updateLayoutForCutout();
         }
@@ -107,6 +114,8 @@ public class PhoneStatusBarView extends FrameLayout {
         super.onDetachedFromWindow();
         Dependency.get(DarkIconDispatcher.class).removeDarkReceiver(mBattery);
         Dependency.get(DarkIconDispatcher.class).removeDarkReceiver(mClock);
+        Dependency.get(DarkIconDispatcher.class).removeDarkReceiver(mClockCentre);
+        Dependency.get(DarkIconDispatcher.class).removeDarkReceiver(mClockRight);
         mDisplayCutout = null;
     }
 
@@ -266,6 +275,15 @@ public class PhoneStatusBarView extends FrameLayout {
                 getPaddingTop(),
                 insets.second,
                 getPaddingBottom());
+
+        // Apply negative paddings to centered area layout so that we'll actually be on the center.
+        final int winRotation = getDisplay().getRotation();
+        LayoutParams centeredAreaParams =
+                (LayoutParams) findViewById(R.id.center_clock_layout).getLayoutParams();
+        centeredAreaParams.leftMargin =
+                winRotation == Surface.ROTATION_0 ? -insets.first : 0;
+        centeredAreaParams.rightMargin =
+                winRotation == Surface.ROTATION_0 ? -insets.second : 0;
     }
 
     /**
