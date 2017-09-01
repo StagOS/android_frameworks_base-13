10f1a3082ced609384b7a36b9169cec128b7a4ab
ezio84 <brabus84@gmail.com>
2017-09-01 15:28:27 +0200

    base: Double tap to sleep on lockscreen [1/2]
    * @jhonboy121: inject SystemSettings utility class and make use of it
    idoybh (YAAP): Add settings value to the backup array
    
    Change-Id: Id8638b0716abf8b264ea9dc2b8dceb892e019aa0

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5305,6 +5305,14 @@ public final class Settings {
         public static final String VOLBTN_MUSIC_CONTROLS = "volbtn_music_controls";
 
         /**
+         * Double tap on lockscreen to sleep
+         * @hide
+         */
+        @Readable
+        public static final String DOUBLE_TAP_SLEEP_LOCKSCREEN = "double_tap_sleep_lockscreen";
+
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
