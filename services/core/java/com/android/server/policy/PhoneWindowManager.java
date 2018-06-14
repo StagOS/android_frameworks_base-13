5cd6db9db174c3491c8e824b4b2c8d076b16cfef
ezio84 <brabus84@gmail.com>
2018-06-13 21:24:55 -0700

    PhoneWindowManager: Allow torch and track skip during ambient display
    This allows long press power button for torch and long press
    volume buttons for track skip to work during ambient display.
    
    sam3000/razorloves: partial pick from:
    
    Author: ezio84 <brabus84@gmail.com>
    Date:   Fri Feb 2 01:24:34 2018 -0500
        base: Support binding the power button to flashlight
        Thanks to beanstown106 for the initial longpress action calls in
        PhoneWindowManager (improved by lineage guys)
        [cut]
        Allow torch action also on ambient display
        Change-Id: I12da044f86c7b625872607529cf8524615cf576b
    
    Author: ezio84 <brabus84@gmail.com>
    Date:   Sun, 7 Jan 2018 21:24:53 +0100
        Fix volume rocker skip track on Ambient Display and Lift to Wake
        we need to check if dream service is dozing before checking
        keyguard status
        Change-Id: Ic3a6c830496188bb6edf27043cd24eb2d553bb82
    
    Change-Id: I2463579e056364652b549524bc9775da4fa35b1f

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1049,7 +1049,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 // Abort possibly stuck animations only when power key up without long press case.
                 mHandler.post(mWindowManagerFuncs::triggerAnimationFailsafe);
                 // See if we deferred screen wake because long press power for torch is enabled
-                if (mResolvedLongPressOnPowerBehavior == LONG_PRESS_POWER_TORCH && !isScreenOn()) {
+                if (mResolvedLongPressOnPowerBehavior == LONG_PRESS_POWER_TORCH &&
+                        (!isScreenOn() || isDozeMode())) {
                     wakeUpFromPowerKey(SystemClock.uptimeMillis());
                 }
             }
@@ -1342,11 +1343,24 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         return mAssistUtils.getAssistComponentForUser(mCurrentUserId) != null;
     }
 
+    private boolean isDozeMode() {
+        IDreamManager dreamManager = getDreamManager();
+
+        try {
+            if (dreamManager != null && dreamManager.isDreaming()) {
+                return true;
+            }
+        } catch (RemoteException e) {
+            Slog.e(TAG, "RemoteException when checking if dreaming", e);
+        }
+        return false;
+    }
+
     private int getResolvedLongPressOnPowerBehavior() {
         if (FactoryTest.isLongPressOnPowerOffEnabled()) {
             return LONG_PRESS_POWER_SHUT_OFF_NO_CONFIRM;
         }
-        if (mTorchLongPressPowerEnabled && !isScreenOn()) {
+        if (mTorchLongPressPowerEnabled && (!isScreenOn() || isDozeMode())) {
             return LONG_PRESS_POWER_TORCH;
         }
 
