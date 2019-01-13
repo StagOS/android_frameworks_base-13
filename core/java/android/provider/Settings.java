84d356521b32d5e7f94e0d776ad59275c60d5d87
Alex Cruz <du.alexcruz@gmail.com>
2019-01-13 10:59:49 -0600

    Bring back traffic indicator for all!
    arrow-edits:
     - adapt to Q

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5409,6 +5409,18 @@ public final class Settings {
         public static final String NOTIFICATION_GUTS_KILL_APP_BUTTON =
                 "notification_guts_kill_app_button";
 
+         /**
+         * Wheter to show network traffic indicator in statusbar
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
+
+        /**
+         * Network traffic inactivity threshold (default is 1 kBs)
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
+
         /**
          * @hide
          */
