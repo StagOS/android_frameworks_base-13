1d818a81277f14c8f31865a34c4fed0b5181972b
SagarMakhar <sagarmakhar@gmail.com>
2019-06-13 10:20:33 +0200

    SystemUI: Add VoLTE icon toggle [1/2]
    Change-Id: I28a981517213509c0a40a815c6e40c695fea6c68
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Amar Bajpai <bajpai.amar3@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4849,6 +4849,12 @@ public final class Settings {
          */
         public static final String SHOW_FOURG = "show_fourg";
 
+	/**
+         * Whether to show VoLTE icon or not
+         * @hide
+         */
+        public static final String SHOW_VOLTE_ICON = "show_volte_icon";
+
         /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
@@ -4982,6 +4988,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(COLOR_BUCKET_OVERLAY);
             PRIVATE_SETTINGS.add(VOLUME_PANEL_ON_LEFT);
             PRIVATE_SETTINGS.add(SHOW_FOURG);
+            PRIVATE_SETTINGS.add(SHOW_VOLTE_ICON);
         }
 
         /**
