6cf787444674b70be2ca423ef187f97aa310dd40
maxwen <max.weninger@gmail.com>
2019-11-27 16:27:40 +0100

    SystemUI: old style mobile data indicators [1/2]
    neobuddy89: Use Tuner API.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5378,6 +5378,11 @@ public final class Settings {
         public static final String TOAST_ICON = "toast_icon";
 
         /**
+         * @hide
+         */
+        public static final String USE_OLD_MOBILETYPE = "use_old_mobiletype";
+
+        /**
          * Whether to display 4G icon instead LTE
          * @hide
          */
