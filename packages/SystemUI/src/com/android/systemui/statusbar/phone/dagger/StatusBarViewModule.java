6c56da6ec1b40f514de34cfe4d7d5ba0674e6efc
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-11-30 13:41:05 +0100

    Battery Styles: Bring back good ol' circle battery style
    * This code is based on the old Java implementation and converted to Kotlin
    
    * Instead of creating different style within the same drawable
      create one on its own, this makes the code more readable and future proof
    
    Anushek Prasal <anushekprasal@gmail.com>
    - Convert to SettingsObserver
    - Cleanup tuner code
    
    CarbonROM:
    - Remove broken battery entry from tuner list
    - Add nullcheck for percentage
    
    Dirty Unicorns:
    - Adapt 'updateShowPercent' method to existing modifications
    - Clean up code
    
    [spezi77] Updated for 12.1.0_r1 (adapt to bvm controller)
    - Skipped "Battery Styles: BatteryPercentage: Detect when overlay changes"
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: I8cc16ed5cb694f577a903bf918616f239a6ea2a6

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/dagger/StatusBarViewModule.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/dagger/StatusBarViewModule.java
@@ -213,7 +213,6 @@ public abstract class StatusBarViewModule {
         return new BatteryMeterViewController(
                 batteryMeterView,
                 configurationController,
-                tunerService,
                 broadcastDispatcher,
                 mainHandler,
                 contentResolver,
