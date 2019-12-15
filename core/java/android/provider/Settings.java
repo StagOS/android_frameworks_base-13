9cfa93b747c0ec8dc36d21f5d96331b57e5253e9
ezio84 <brabus84@gmail.com>
2019-12-15 10:38:37 +0100

    Gestures: Allow to set back swipe deadzone [1/2]
    so it's easier to open apps left drawer or avoid unwanted back swipes
    when using gallery apps
    
    Change-Id: I805941e7b9de44f3ffdb115ccaad9fa8c0712576

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5320,6 +5320,16 @@ public final class Settings {
         public static final String RINGTONE_VIBRATION_PATTERN = "ringtone_vibration_pattern";
 
         /**
+         * Set edge gesture Y dead zone
+         * 0 - disabled - stock behavior - default
+         * 1 - smaller top area of the screen
+         * 2 - small top area of the screen
+         * 3 - half top of the screen
+         * @hide
+         */
+        public static final String EDGE_GESTURE_Y_DEAD_ZONE = "edge_gesture_y_dead_zone";
+
+        /**
          * Whether to show the battery info on the lockscreen while charging
          * @hide
          */
