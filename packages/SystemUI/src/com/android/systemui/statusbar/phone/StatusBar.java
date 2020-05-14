9766a2d663a309638a46b45b072d1b55fc04c87b
Anushek Prasal <anushekprasal@gmail.com>
2020-05-14 23:20:31 +0530

    SystemUI: Improve Network Traffic
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Change-Id: Ic5a17979f9adbe761c8c5e3d5130004a90430daf
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -2034,7 +2034,6 @@ public class StatusBar extends SystemUI implements
         if (!isExpanded) {
             mRemoteInputManager.onPanelCollapsed();
         }
-        ((StatusBarIconControllerImpl) mStatusBarIconController).onPanelExpanded(isExpanded);
     }
 
     public ViewGroup getNotificationScrollLayout() {
