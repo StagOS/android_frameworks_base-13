44a35885d8b8f80b3b20df1eb05685e3a872fda2
Danny Baumann <dannybaumann@web.de>
2020-11-05 19:13:14 +0000

    Add back increasing ring feature [1/3]
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Change-Id: I2cd34e08badb343fa418ed07e6733e3fbe7d7d5d
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5551,6 +5551,24 @@ public final class Settings {
         public static final String VIBRATE_ON_DISCONNECT = "vibrate_on_disconnect";
 
         /**
+         * Whether the phone ringtone should be played in an increasing manner
+         * @hide
+         */
+        public static final String INCREASING_RING = "increasing_ring";
+
+        /**
+         * Start volume fraction for increasing ring volume
+         * @hide
+         */
+        public static final String INCREASING_RING_START_VOLUME = "increasing_ring_start_vol";
+
+        /**
+         * Ramp up time (seconds) for increasing ring
+         * @hide
+         */
+        public static final String INCREASING_RING_RAMP_UP_TIME = "increasing_ring_ramp_up_time";
+
+        /**
          * These are all public system settings
          *
          * @hide
