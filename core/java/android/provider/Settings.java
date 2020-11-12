1af8767cd391f7c76eca779f2004c7daacea4ed6
Dil3mm4 <dil3mm4.dev@gmail.com>
2020-11-12 14:30:29 +0000

    Feature: reticker [1/2]
    Change-Id: I582d0484977f0e850d158e8645171e638fcc0c4c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6002,6 +6002,10 @@ public final class Settings {
          */
         public static final String ALLOW_INCALL_HOME = "allow_incall_home";
 
+        /** reTicker **/
+        public static final String RETICKER_STATUS = "reticker_status";
+        public static final String RETICKER_COLORED = "reticker_colored";
+
         /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
