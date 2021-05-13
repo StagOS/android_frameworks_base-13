2df7168b6d9178a1e408c12f9619d1e2bc6aff11
vjspranav <pranavasri@live.in>
2021-05-13 21:56:35 +0200

    Add gradient Switch style [2/3]

--- core/java/com/android/internal/util/stag/ThemesUtils.java
+++ core/java/com/android/internal/util/stag/ThemesUtils.java
@@ -41,6 +41,7 @@ public class ThemesUtils {
         "com.android.system.switch.md2", // 5
         "com.android.system.switch.retro", // 6
         "com.android.system.switch.stockish", //7
+        "com.android.system.switch.gradient", //8
     };
 
     public static void updateSwitchStyle(IOverlayManager om, int userId, int switchStyle) {
