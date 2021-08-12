yaap/frameworks_base

FODCircleViewImpl: Refuse to start if not enabled
Change-Id: Ibaaac09535e572145fce83d548ea7b9a3d869b1e

packages/SystemUI/src/com/android/systemui/biometrics/FODCircleViewImpl.java
@@ -35,15 +35,18 @@ public class FODCircleViewImpl extends SystemUI implements CommandQueue.Callback
 
     private FODCircleView mFodCircleView;
     private final CommandQueue mCommandQueue;
+    private final boolean mIsEnabled;
 
     @Inject
     public FODCircleViewImpl(Context context, CommandQueue commandQueue) {
         super(context);
         mCommandQueue = commandQueue;
+        mIsEnabled = context.getResources().getBoolean(com.android.internal.R.bool.config_needCustomFODView);
     }
 
     @Override
     public void start() {
+        if (!mIsEnabled) return;
         PackageManager packageManager = mContext.getPackageManager();
         if (!packageManager.hasSystemFeature(PackageManager.FEATURE_FINGERPRINT)) {
             return;

StagOS/android_frameworks_base-13

Signed-off-by: aswin7469 <aswinas@pixysos.com>

packages/SystemUI/src/com/android/systemui/biometrics/FODCircleViewImpl.java
@@ -34,15 +34,18 @@ public class FODCircleViewImpl extends SystemUI implements CommandQueue.Callback
 
     private FODCircleView mFodCircleView;
     private final CommandQueue mCommandQueue;
+    private final boolean mIsEnabled;
 
     @Inject
     public FODCircleViewImpl(Context context, CommandQueue commandQueue) {
         super(context);
         mCommandQueue = commandQueue;
+        mIsEnabled = context.getResources().getBoolean(com.android.internal.R.bool.config_supportsInDisplayFingerprint);
     }
 
     @Override
     public void start() {
+        if (!mIsEnabled) return;
         PackageManager packageManager = mContext.getPackageManager();
         if (!packageManager.hasSystemFeature(PackageManager.FEATURE_FINGERPRINT)) {
             return;