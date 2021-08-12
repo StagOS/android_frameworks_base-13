TenX-OS/frameworks_base

SystemUI: Add 5 more Custom Statusbar Signal & Wi-Fi icons [1/2]
- Thanks to sis, acherus, iconicicons & cool_modules_69 for these beautiful icons.

Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

core/java/com/android/internal/util/tenx/ThemesUtils.java
@@ -132,6 +132,11 @@ public class ThemesUtils {
         "com.tenx.systemui.signalbar_a",
         "com.tenx.systemui.signalbar_b",
         "com.tenx.systemui.signalbar_c",
+        "com.tenx.systemui.signalbar_d",
+        "com.tenx.systemui.signalbar_e",
+        "com.tenx.systemui.signalbar_f",
+        "com.tenx.systemui.signalbar_g",
+        "com.tenx.systemui.signalbar_h",
     };
 
     // Statusbar Wifi icons
@@ -139,6 +144,11 @@ public class ThemesUtils {
         "com.tenx.systemui.wifibar_a",
         "com.tenx.systemui.wifibar_b",
         "com.tenx.systemui.wifibar_c",
+        "com.tenx.systemui.wifibar_d",
+        "com.tenx.systemui.wifibar_e",
+        "com.tenx.systemui.wifibar_f",
+        "com.tenx.systemui.wifibar_g",
+        "com.tenx.systemui.wifibar_h",
     };
 
     public String[] getTheme(int theme) {

StagOS/android_frameworks_base-13

Signed-off-by: Jackeagle <jackeagle102@gmail.com>
Change-Id: I10dc037f2709ce41ba1b451312ee20dfdcf1b799
Signed-off-by: Cruzer1 <preetamswain143@gmail.com>

core/java/com/android/internal/util/stag/ThemesUtils.java
@@ -52,6 +52,11 @@ public class ThemesUtils {
         "org.blissroms.systemui.signalbar_a",
         "org.blissroms.systemui.signalbar_b",
         "org.blissroms.systemui.signalbar_c",
+        "org.blissroms.systemui.signalbar_d",
+        "org.blissroms.systemui.signalbar_e",
+        "org.blissroms.systemui.signalbar_f",
+        "org.blissroms.systemui.signalbar_g",
+        "org.blissroms.systemui.signalbar_h",
     };
 
     // Statusbar Wifi icons
@@ -59,6 +64,11 @@ public class ThemesUtils {
         "org.blissroms.systemui.wifibar_a",
         "org.blissroms.systemui.wifibar_b",
         "org.blissroms.systemui.wifibar_c",
+        "org.blissroms.systemui.wifibar_d",
+        "org.blissroms.systemui.wifibar_e",
+        "org.blissroms.systemui.wifibar_f",
+        "org.blissroms.systemui.wifibar_g",
+        "org.blissroms.systemui.wifibar_h",
     };
 
     public static void updateSwitchStyle(IOverlayManager om, int userId, int switchStyle) {