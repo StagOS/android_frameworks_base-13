syberia-project/platform_frameworks_base

LongSwipe: add partial screenshot action [1/2]
Signed-off-by: DennySPb <dennyspb@gmail.com>
Change-Id: I9f021dd99c7b3cf0925b9ddf2f8e4c45db41f19a
Signed-off-by: DennySPb <dennyspb@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
@@ -791,6 +791,9 @@ public class EdgeBackGestureHandler extends CurrentUserTracker implements Displa
             case 14: // Switch recent app
                 ActionUtils.switchToLastApp(mContext);
                 break;
+            case 15: // Partial screenshot
+                SyberiaUtils.takeScreenshot(false);
+                break;
         }
     }
 

StagOS/android_frameworks_base-13

packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
@@ -891,6 +891,9 @@ public class EdgeBackGestureHandler extends CurrentUserTracker implements Displa
             case 14: // Switch recent app
                 ActionUtils.switchToLastApp(mContext);
                 break;
+            case 15: // Partial screenshot
+                StagUtils.takeScreenshot(false);
+                break;
         }
     }
 