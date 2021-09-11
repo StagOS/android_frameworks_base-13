syberia-project/platform_frameworks_base

LongSwipe: change helper for screenshot action
Signed-off-by: DennySPb <dennyspb@gmail.com>
Change-Id: I35090be013d76d6d5eb740ee48bd543e2bbf1cbe

packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
@@ -771,7 +771,7 @@ public class EdgeBackGestureHandler extends CurrentUserTracker implements Displa
                 ActionUtils.switchScreenOff(mContext);
                 break;
             case 8: // Screenshot
-                ActionUtils.takeScreenshot(true);
+                SyberiaUtils.takeScreenshot(true);
                 break;
             case 9: // Notification panel
                 ActionUtils.toggleNotifications();

StagOS/android_frameworks_base-13

packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
@@ -871,7 +871,7 @@ public class EdgeBackGestureHandler extends CurrentUserTracker implements Displa
                 ActionUtils.switchScreenOff(mContext);
                 break;
             case 8: // Screenshot
-                ActionUtils.takeScreenshot(true);
+                StagUtils.takeScreenshot(true);
                 break;
             case 9: // Notification panel
                 ActionUtils.toggleNotifications();