5eee57cbbeee527137ea9bfed8e2acd39735bc16
Ratoriku <a1063021545@gmail.com>
2021-10-23 18:01:56 +0800

    PixelPropsUtils: Add chrome into extra packages list
    Signed-off-by: Ratoriku <a1063021545@gmail.com>
    Change-Id: I09413e803287f00fbb56924edf6c73f9286d9cc7

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -44,6 +44,7 @@ public class PixelPropsUtils {
     private static final Map<String, Object> propsToChangePixelXL;
     private static final Map<String, ArrayList<String>> propsToKeep;
     private static final String[] extraPackagesToChange = {
+        "com.android.chrome",
         "com.android.vending",
         "com.breel.wallpapers20",
         "com.snapchat.android"
