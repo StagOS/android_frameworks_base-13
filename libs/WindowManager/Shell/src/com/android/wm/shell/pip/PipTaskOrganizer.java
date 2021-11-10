bf6e4a3f624d512ca7549b944a64233fbebd5bd4
jhonboy121 <alfredmathew05@gmail.com>
2021-11-10 17:04:20 +0530

    libs: PipTaskOrganizer: do not start fade out animation for pip if surfacecontrol leash is null
    * and here's the stack trace
    
    E/AndroidRuntime( 2321): FATAL EXCEPTION: main
    E/AndroidRuntime( 2321): Process: com.android.systemui, PID: 2321
    E/AndroidRuntime( 2321): java.lang.NullPointerException: Attempt to invoke direct method 'void android.view.SurfaceControl.checkNotReleased()' on a null object reference
    E/AndroidRuntime( 2321): 	at android.view.SurfaceControl.access$2600(SurfaceControl.java:87)
    E/AndroidRuntime( 2321): 	at android.view.SurfaceControl$Transaction.checkPreconditions(SurfaceControl.java:2597)
    E/AndroidRuntime( 2321): 	at android.view.SurfaceControl$Transaction.setMatrix(SurfaceControl.java:2907)
    E/AndroidRuntime( 2321): 	at android.view.SurfaceControl$Transaction.setMatrix(SurfaceControl.java:2925)
    E/AndroidRuntime( 2321): 	at com.android.wm.shell.pip.PipSurfaceTransactionHelper.resetScale(PipSurfaceTransactionHelper.java:173)
    E/AndroidRuntime( 2321): 	at com.android.wm.shell.pip.PipAnimationController$PipTransitionAnimator$1.onStartTransaction(PipAnimationController.java:502)
    E/AndroidRuntime( 2321): 	at com.android.wm.shell.pip.PipAnimationController$PipTransitionAnimator.onAnimationStart(PipAnimationController.java:282)
    E/AndroidRuntime( 2321): 	at android.animation.Animator$AnimatorListener.onAnimationStart(Animator.java:539)
    E/AndroidRuntime( 2321): 	at android.animation.ValueAnimator.notifyStartListeners(ValueAnimator.java:1031)
    E/AndroidRuntime( 2321): 	at android.animation.ValueAnimator.startAnimation(ValueAnimator.java:1280)
    E/AndroidRuntime( 2321): 	at android.animation.ValueAnimator.start(ValueAnimator.java:1082)
    E/AndroidRuntime( 2321): 	at android.animation.ValueAnimator.start(ValueAnimator.java:1106)
    E/AndroidRuntime( 2321): 	at com.android.wm.shell.pip.PipTaskOrganizer.fadeExistingPip(PipTaskOrganizer.java:829)
    E/AndroidRuntime( 2321): 	at com.android.wm.shell.pip.PipTaskOrganizer.onFixedRotationStarted(PipTaskOrganizer.java:789)
    E/AndroidRuntime( 2321): 	at com.android.wm.shell.common.DisplayController.onFixedRotationStarted(DisplayController.java:202)
    E/AndroidRuntime( 2321): 	at com.android.wm.shell.common.DisplayController.access$400(DisplayController.java:43)
    E/AndroidRuntime( 2321): 	at com.android.wm.shell.common.DisplayController$DisplayWindowListenerImpl.lambda$onFixedRotationStarted$3(DisplayController.java:253)
    E/AndroidRuntime( 2321): 	at com.android.wm.shell.common.DisplayController$DisplayWindowListenerImpl.$r8$lambda$B-7e2BwvPJc-rkxPeEPH3nk4V9w(Unknown Source:0)
    E/AndroidRuntime( 2321): 	at com.android.wm.shell.common.DisplayController$DisplayWindowListenerImpl$$ExternalSyntheticLambda3.run(Unknown Source:6)
    E/AndroidRuntime( 2321): 	at android.os.Handler.handleCallback(Handler.java:938)
    E/AndroidRuntime( 2321): 	at android.os.Handler.dispatchMessage(Handler.java:99)
    E/AndroidRuntime( 2321): 	at android.os.Looper.loopOnce(Looper.java:201)
    E/AndroidRuntime( 2321): 	at android.os.Looper.loop(Looper.java:288)
    E/AndroidRuntime( 2321): 	at android.app.ActivityThread.main(ActivityThread.java:7840)
    E/AndroidRuntime( 2321): 	at java.lang.reflect.Method.invoke(Native Method)
    E/AndroidRuntime( 2321): 	at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:550)
    E/AndroidRuntime( 2321): 	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:1003)
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- libs/WindowManager/Shell/src/com/android/wm/shell/pip/PipTaskOrganizer.java
+++ libs/WindowManager/Shell/src/com/android/wm/shell/pip/PipTaskOrganizer.java
@@ -62,6 +62,7 @@ import android.content.res.Configuration;
 import android.graphics.Rect;
 import android.os.RemoteException;
 import android.os.SystemClock;
+import android.util.Log;
 import android.view.Display;
 import android.view.Surface;
 import android.view.SurfaceControl;
@@ -952,12 +953,16 @@ public class PipTaskOrganizer implements ShellTaskOrganizer.TaskListener,
         }
         final float alphaStart = show ? 0 : 1;
         final float alphaEnd = show ? 1 : 0;
-        mPipAnimationController
+        if (mLeash != null) {
+            mPipAnimationController
                 .getAnimator(mTaskInfo, mLeash, mPipBoundsState.getBounds(), alphaStart, alphaEnd)
                 .setTransitionDirection(TRANSITION_DIRECTION_SAME)
                 .setPipTransactionHandler(mPipTransactionHandler)
                 .setDuration(show ? mEnterAnimationDuration : mExitAnimationDuration)
                 .start();
+        } else {
+            Log.w(TAG, "mLeash is null, skipping animation");
+        }
         mHasFadeOut = !show;
     }
 
