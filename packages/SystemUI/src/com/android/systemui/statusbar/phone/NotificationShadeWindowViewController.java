1a1c871b844e302b66afc0feec7311f8ada82b0a
Ido Ben-Hur <idoybh2@gmail.com>
2021-11-26 15:21:20 +0200

    BrightnessMirrorController: Don't show auto brightness icon when it's disabled
    Also remove other junk from all around that commit (https://github.com/yaap/frameworks_base/commit/0da42b4c536ab82377f9b2d23b6f156daac9ca13)
    That includes but not limited to:
    * Misuse of boolean vars
    * Unused public functions
    * Annoying reversed logic
    * Redundant global vars
    * Pointless & Incorrect null checks

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationShadeWindowViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationShadeWindowViewController.java
@@ -114,9 +114,6 @@ public class NotificationShadeWindowViewController {
     private final SuperStatusBarViewFactory mStatusBarViewFactory;
     private final SystemSettings mSystemSettings;
 
-    private ImageView mAutoBrightnessIcon;
-    private boolean mShowAutoBrightnessButton;
-
     // Used for determining view / touch intersection
     private int[] mTempLocation = new int[2];
     private RectF mTempRect = new RectF();
@@ -178,10 +175,6 @@ public class NotificationShadeWindowViewController {
         mSystemSettings = systemSettings;
         // This view is not part of the newly inflated expanded status bar.
         mBrightnessMirror = mView.findViewById(R.id.brightness_mirror_container);
-        mAutoBrightnessIcon = (ImageView)
-                mBrightnessMirror.findViewById(R.id.brightness_icon);
-        mShowAutoBrightnessButton = mTunerService.getValue(
-                QS_SHOW_AUTO_BRIGHTNESS_BUTTON, 1) == 1;
     }
 
     /** Inflates the {@link R.layout#status_bar_expanded} layout and sets it up. */
@@ -441,10 +434,11 @@ public class NotificationShadeWindowViewController {
             public void onChildViewAdded(View parent, View child) {
                 if (child.getId() == R.id.brightness_mirror_container) {
                     mBrightnessMirror = child;
-                    mAutoBrightnessIcon = (ImageView)
+                    ImageView autoBrightnessIcon =
                             child.findViewById(R.id.brightness_icon);
-                    mAutoBrightnessIcon.setVisibility(!mShowAutoBrightnessButton
-                            ? View.GONE : View.VISIBLE);
+                    boolean show = mTunerService.getValue(
+                            QS_SHOW_AUTO_BRIGHTNESS_BUTTON, 1) == 1;
+                    autoBrightnessIcon.setVisibility(show ? View.VISIBLE : View.GONE);
                 }
             }
 
