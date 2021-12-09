bfc9062f4297418b7b2d14a5b4aff9d8dc352696
spkal01 <spkal01@users.noreply.github.com>
2021-12-09 18:32:21 +0000

    Make colored Statusbar Icons optional [1/2]
    Change-Id: Ided98ca042695cb77570945a377d3f0fa1fa4476
    Signed-off-by: blinoff82 <blinov.in@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationIconAreaController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationIconAreaController.java
@@ -1,5 +1,7 @@
 package com.android.systemui.statusbar.phone;
 
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.res.Resources;
@@ -90,6 +92,7 @@ public class NotificationIconAreaController implements
 
     private int mAodIconAppearTranslation;
 
+    boolean NewIconStyle;
     private boolean mAnimationsEnabled;
     private int mAodIconTint;
     private boolean mAodIconsVisible;
@@ -138,6 +141,9 @@ public class NotificationIconAreaController implements
         initializeNotificationAreaViews(context);
         reloadAodColor();
         darkIconDispatcher.addDarkReceiver(this);
+
+        NewIconStyle = Settings.System.getIntForUser(mContext.getContentResolver(),
+            Settings.System.STATUSBAR_ICONS_STYLE, 1, UserHandle.USER_CURRENT) == 1;
     }
 
     protected View inflateIconArea(LayoutInflater inflater) {
@@ -488,7 +494,7 @@ public class NotificationIconAreaController implements
         if (colorize) {
             color = DarkIconDispatcher.getTint(mTintAreas, v, tint);
         }
-        if (v.getStatusBarIcon().pkg.contains("systemui")) {
+        if (v.getStatusBarIcon().pkg.contains("systemui") || !NewIconStyle) {
             v.setStaticDrawableColor(color);
             v.setDecorColor(tint);
         } else {
