b8d32b7949fe9cab4ae0ef21ccb1fed49628b55a
SagarMakhar <sagarmakhar@gmail.com>
2021-12-29 08:16:18 +0000

    BiometricScheduler: Cancel operation if not idle
    - some hals fail to report success/failure (for ex. realme fp hals)
    [DarkJoker360 - Switch to overlays]
    
    - adapted for qpr3
    
    Co-authored-by: Dmitrii <bankersenator@gmail.com>
    Co-authored-by: Hưng Phan <phandinhhungvp2001@gmail.com>
    Change-Id: I442ce063280af36a04c25fcbc3dd45a90f196988
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- services/core/java/com/android/server/biometrics/sensors/BiometricScheduler.java
+++ services/core/java/com/android/server/biometrics/sensors/BiometricScheduler.java
@@ -248,7 +248,7 @@ public class BiometricScheduler {
                         ServiceManager.getService(Context.BIOMETRIC_SERVICE)),
                 LOG_NUM_RECENT_OPERATIONS, CoexCoordinator.getInstance());
         mCancel = context.getResources().getBoolean(
-                 com.android.internal.R.bool.config_fpCancelIfNotIdle);
+                com.android.internal.R.bool.config_fpCancelIfNotIdle);
     }
 
     @VisibleForTesting
@@ -262,12 +262,14 @@ public class BiometricScheduler {
 
     protected void startNextOperationIfIdle() {
         if (mCurrentOperation != null) {
-            if(mCancel) {
-               Slog.v(getTag(), "Not idle, cancelling current operation: " + mCurrentOperation);
-               mCurrentOperation.cancel(mHandler, mInternalCallback);
+            if (mCancel) {
+                if (!mCurrentOperation.isFinished()) {
+                    Slog.v(getTag(), "Not idle, cancelling current operation: " + mCurrentOperation);
+                    mCurrentOperation.cancel(mHandler, mInternalCallback);
+                }
             } else {
-               Slog.v(getTag(), "Not idle, current operation: " + mCurrentOperation);
-               return;
+                Slog.v(getTag(), "Not idle, current operation: " + mCurrentOperation);
+                return;
             }
         }
         if (mPendingOperations.isEmpty()) {
