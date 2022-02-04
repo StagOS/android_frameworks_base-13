323ab6ecf21207dc9c40af857470ac091a1d115c
aswin7469 <aswinas@pixysos.com>
2022-02-04 05:33:09 +0530

    SystemUI: stop skipping animation while unlocking
    * after r27 this cauing flickering with udfps
    
    This reverts commit bf146b3be986bf2c298b41eb88ea50349dc4504a. &  c8bf4f1
    
    Change-Id: Id1ea45047c57aaeef5b057817c2d2c3c89b4c44b

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -3942,7 +3942,7 @@ public class StatusBar extends SystemUI implements
 
         boolean launchingAffordanceWithPreview =
                 mNotificationPanelViewController.isLaunchingAffordanceWithPreview();
-        mScrimController.setLaunchingAffordanceWithPreview(true);
+        mScrimController.setLaunchingAffordanceWithPreview(launchingAffordanceWithPreview);
 
         if (mStatusBarKeyguardViewManager.isShowingAlternateAuth()) {
             if (mState == StatusBarState.SHADE || mState == StatusBarState.SHADE_LOCKED) {
