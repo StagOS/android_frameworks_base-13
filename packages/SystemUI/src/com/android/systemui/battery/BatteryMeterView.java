837206e1c9a05f5d366f79037653cb45fb56ec60
vjspranav <pranavasri@live.in>
2022-03-27 18:54:56 +0530

    Revert "Battery Styles: BatteryPercentage: Detect when overlay changes"
    This reverts commit 805cc64fadce8201d8e4de0d9801b0530a32d770.
    
    Change-Id: I5fd5b458947588e003a3c638b1b7e5cacf8ed851

--- packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
@@ -426,11 +426,6 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
         updateShowPercent();
     }
 
-    @Override
-    public void onOverlayChanged() {
-        updateShowPercent();
-    }
-
     /**
      * Looks up the scale factor for status bar icons and scales the battery view by that amount.
      */
