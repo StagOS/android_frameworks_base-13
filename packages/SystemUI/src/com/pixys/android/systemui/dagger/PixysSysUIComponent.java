06013f0df967bee5748b44d924d8aea13578f7eb
vjspranav <pranavasri@live.in>
2022-03-29 18:19:37 +0530

    Revert "SystemUI: Integrate smartspace and backgesture provider"
    This reverts commit 2b6228e2253d45776ec13738424a54d3239aa2e3.

--- packages/SystemUI/src/com/pixys/android/systemui/dagger/PixysSysUIComponent.java
+++ packages/SystemUI/src/com/pixys/android/systemui/dagger/PixysSysUIComponent.java
@@ -7,9 +7,6 @@ import com.android.systemui.dagger.SystemUIBinder;
 import com.android.systemui.dagger.SysUIComponent;
 import com.android.systemui.dagger.SystemUIModule;
 
-import com.pixys.android.systemui.keyguard.PixysKeyguardSliceProvider;
-import com.pixys.android.systemui.smartspace.KeyguardSmartspaceController;
-
 import dagger.Subcomponent;
 
 @SysUISingleton
@@ -25,12 +22,4 @@ public interface PixysSysUIComponent extends SysUIComponent {
     interface Builder extends SysUIComponent.Builder {
         PixysSysUIComponent build();
     }
-
-    /**
-     * Member injection into the supplied argument.
-     */
-    void inject(PixysKeyguardSliceProvider keyguardSliceProvider);
-
-    @SysUISingleton
-    KeyguardSmartspaceController createKeyguardSmartspaceController();
 }
