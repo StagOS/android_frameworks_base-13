Project-Xtended/frameworks_base

PowerManagerService: Send broadcast when going to sleep
Signed-off-by: Mukesh Singh <mks22584@gmail.com>

core/java/com/android/internal/util/xtended/content/Intent.java
@@ -1,0 +1,15 @@
+package com.android.internal.util.xtended.content;
+import android.Manifest;
+
+/**
+ * crDroid specific intent definition class.
+ */
+public class Intent {
+
+    /**
+     * Broadcast action: go to sleep
+     * @hide
+     */
+    public static final String ACTION_GO_TO_SLEEP =
+            "xtended.intent.action.GO_TO_SLEEP";
+}

services/core/java/com/android/server/power/PowerManagerService.java
@@ -2030,7 +2030,15 @@ public final class PowerManagerService extends SystemService
         } finally {
             Trace.traceEnd(Trace.TRACE_TAG_POWER);
         }
+        mHandler.post(() -> sendSleepBroadcast());
         return true;
+    }
+
+    private void sendSleepBroadcast() {
+        Intent intent = new Intent(
+                com.android.internal.util.xtended.content.Intent.ACTION_GO_TO_SLEEP);
+        intent.setFlags(Intent.FLAG_RECEIVER_INCLUDE_BACKGROUND);
+        mContext.sendBroadcastAsUser(intent, UserHandle.SYSTEM);
     }
 
     private void napInternal(long eventTime, int uid) {

StagOS/android_frameworks_base-13

core/java/com/android/internal/util/stag/content/Intent.java
@@ -1,0 +1,15 @@
+package com.android.internal.util.stag.content;
+import android.Manifest;
+
+/**
+ * crDroid specific intent definition class.
+ */
+public class Intent {
+
+    /**
+     * Broadcast action: go to sleep
+     * @hide
+     */
+    public static final String ACTION_GO_TO_SLEEP =
+            "stag.intent.action.GO_TO_SLEEP";
+}

services/core/java/com/android/server/power/PowerManagerService.java
@@ -1906,9 +1906,17 @@ public final class PowerManagerService extends SystemService
         } finally {
             Trace.traceEnd(Trace.TRACE_TAG_POWER);
         }
+        mHandler.post(() -> sendSleepBroadcast());
         return true;
     }
-
+    
+    private void sendSleepBroadcast() {
+        Intent intent = new Intent(
+                com.android.internal.util.stag.content.Intent.ACTION_GO_TO_SLEEP);
+        intent.setFlags(Intent.FLAG_RECEIVER_INCLUDE_BACKGROUND);
+        mContext.sendBroadcastAsUser(intent, UserHandle.SYSTEM);
+    }
+    
     private void dreamDisplayGroup(int groupId, long eventTime, int uid) {
         synchronized (mLock) {
             if (dreamDisplayGroupNoUpdateLocked(groupId, eventTime, uid)) {