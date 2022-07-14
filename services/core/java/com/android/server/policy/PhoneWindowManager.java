88e6e2a705d91f1c9cdbf3fdb11ec42936c0e481
vjspranav <pranavasri@live.in>
2022-07-14 19:51:30 +0200

    Revert "policy: Add support for device alert sliders"
    This reverts commit 7d8a8ee7bc38c9c6766c2a58485e7d38e59967b7.

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -144,7 +144,6 @@ import android.media.AudioManagerInternal;
 import android.media.AudioSystem;
 import android.media.IAudioService;
 import android.media.session.MediaSessionLegacyHelper;
-import android.net.Uri;
 import android.os.Binder;
 import android.os.Bundle;
 import android.os.DeviceIdleManager;
@@ -177,7 +176,6 @@ import android.service.dreams.IDreamManager;
 import android.service.vr.IPersistentVrStateCallbacks;
 import android.speech.RecognizerIntent;
 import android.telecom.TelecomManager;
-import android.text.TextUtils;
 import android.util.Log;
 import android.util.MutableBoolean;
 import android.util.PrintWriterPrinter;
@@ -414,7 +412,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     AppOpsManager mAppOpsManager;
     PackageManager mPackageManager;
     SideFpsEventHandler mSideFpsEventHandler;
-    AlertSliderObserver mAlertSliderObserver;
     private boolean mHasFeatureAuto;
     private boolean mHasFeatureWatch;
     private boolean mHasFeatureLeanback;
@@ -713,7 +710,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private static final int MSG_RINGER_TOGGLE_CHORD = 24;
     private static final int MSG_DISPATCH_VOLKEY_WITH_WAKE_LOCK = 29;
 
-    private boolean mHasAlertSlider = false;
     private SwipeToScreenshotListener mSwipeToScreenshot;
 
     private static final int MSG_TOGGLE_TORCH = 100;
@@ -823,9 +819,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     };
 
     class SettingsObserver extends ContentObserver {
-        private final Uri SWAP_ALERT_SLIDER_ORDER_URI =
-                Settings.System.getUriFor(Settings.System.ALERT_SLIDER_ORDER);
-
         SettingsObserver(Handler handler) {
             super(handler);
         }
@@ -871,9 +864,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.Global.getUriFor(
                     Settings.Global.POWER_BUTTON_SUPPRESSION_DELAY_AFTER_GESTURE_WAKE), false, this,
-                    UserHandle.USER_ALL);
-            resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.ALERT_SLIDER_ORDER), false, this,
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.THREE_FINGER_GESTURE), false, this,
@@ -953,15 +943,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             updateSettings();
         }
 
-        @Override
-        public void onChange(boolean selfChange, Uri uri) {
-            if (SWAP_ALERT_SLIDER_ORDER_URI.equals(uri)
-                    && mSystemReady && mAlertSliderObserver != null) {
-                mAlertSliderObserver.update();
-            } else {
-                updateSettings();
-                updateRotation(false);
-            }
+        @Override public void onChange(boolean selfChange) {
+            updateSettings();
+            updateRotation(false);
         }
     }
 
@@ -1979,11 +1963,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         Resources res = mContext.getResources();
         mWakeOnDpadKeyPress =
                 res.getBoolean(com.android.internal.R.bool.config_wakeOnDpadKeyPress);
-
-        // Init alert slider
-        mHasAlertSlider = mContext.getResources().getBoolean(R.bool.config_hasAlertSlider)
-                && !TextUtils.isEmpty(mContext.getResources().getString(R.string.alert_slider_state_path))
-                && !TextUtils.isEmpty(mContext.getResources().getString(R.string.alert_slider_uevent_match_path));
 
         // Init display burn-in protection
         boolean burnInProtectionEnabled = context.getResources().getBoolean(
@@ -5727,11 +5706,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         mVrManagerInternal = LocalServices.getService(VrManagerInternal.class);
         if (mVrManagerInternal != null) {
             mVrManagerInternal.addPersistentVrModeStateListener(mPersistentVrModeListener);
-        }
-
-        if (mHasAlertSlider) {
-            mAlertSliderObserver = new AlertSliderObserver(mContext);
-            mAlertSliderObserver.startObserving(com.android.internal.R.string.alert_slider_uevent_match_path);
         }
 
         mLineageHardware = LineageHardwareManager.getInstance(mContext);
