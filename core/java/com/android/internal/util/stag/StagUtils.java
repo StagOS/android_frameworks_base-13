b1065dc690a23e48985d5d9260b6801aa586e07c
vjspranav <pranavasri@live.in>
2022-09-10 20:03:48 +0000

    StagUtils: Temporarily remove flashlight api

--- core/java/com/android/internal/util/stag/StagUtils.java
+++ core/java/com/android/internal/util/stag/StagUtils.java
@@ -104,10 +104,6 @@ public class StagUtils {
         return ctx.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
     }
 
-    public static void toggleCameraFlash() {
-        FireActions.toggleCameraFlash();
-    }
-
     public static void sendKeycode(int keycode) {
         long when = SystemClock.uptimeMillis();
         final KeyEvent evDown = new KeyEvent(when, when, KeyEvent.ACTION_DOWN, keycode, 0,
@@ -157,17 +153,6 @@ public class StagUtils {
                             ServiceManager.getService("statusbar"));
                 }
                 return mStatusBarService;
-            }
-        }
-
-        public static void toggleCameraFlash() {
-            IStatusBarService service = getStatusBarService();
-            if (service != null) {
-                try {
-                    service.toggleCameraFlash();
-                } catch (RemoteException e) {
-                    // do nothing.
-                }
             }
         }
     }
