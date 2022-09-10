6c68b9a93e9ca4297779b21b32c0c816e91490d8
vjspranav <pranavasri@live.in>
2022-09-10 19:12:49 +0000

    Remove OneHandedGestureOverlay

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -74,7 +74,6 @@ LOCAL_REQUIRED_MODULES := \
 	NavigationBarModeGesturalOverlayNarrowBack \
 	NavigationBarModeGesturalOverlayWideBack \
 	NavigationBarModeGesturalOverlayExtraWideBack \
-	OneHandedModeGesturalOverlay \
 	preinstalled-packages-platform-overlays.xml
 
 include $(BUILD_PHONY_PACKAGE)
