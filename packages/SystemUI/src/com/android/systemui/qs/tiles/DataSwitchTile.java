33e7554f853a8312ccc54c9da28add62b0181d24
vjspranav <pranavasri@live.in>
2022-09-11 00:07:20 +0000

    DataSwitchTile: Remove Deprecated method

--- packages/SystemUI/src/com/android/systemui/qs/tiles/DataSwitchTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/DataSwitchTile.java
@@ -217,13 +217,6 @@ public class DataSwitchTile extends QSTileImpl<BooleanState> {
         return MetricsEvent.HORNS;
     }
 
-    @Override
-    protected String composeChangeAnnouncement() {
-        return mContext.getString(mState.value
-                ? R.string.qs_data_switch_changed_1
-                : R.string.qs_data_switch_changed_2);
-    }
-
     /**
      * Set whether to enable data for {@code subId}, also whether to disable data for other
      * subscription
