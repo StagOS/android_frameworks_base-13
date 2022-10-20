12067837c7b814dd31754efa311de22a7353c221
vjspranav <pranavasri@live.in>
2022-10-20 10:33:49 +0000

    Disable QuickQS pull down by default

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -5822,7 +5822,7 @@ public final class NotificationPanelViewController implements Dumpable {
             switch (key) {
                 case STATUS_BAR_QUICK_QS_PULLDOWN:
                     mOneFingerQuickSettingsIntercept =
-                            TunerService.parseInteger(newValue, 1);
+                            TunerService.parseInteger(newValue, 0);
                     break;
                 default:
                     break;
