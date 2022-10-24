269b319144017a1cd0888ccaf859567c4fcc8fbc
althafvly <althafvly@gmail.com>
2022-10-24 03:26:35 +0000

    Keyguard: Allow user configurable fingerprint wake-and-unlock
    Change-Id: I471908cfa27a54e0075394eed348cb15386f62ce

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5730,6 +5730,12 @@ public final class Settings {
         public static final String DOZE_TRIGGER_DOUBLETAP = "doze_trigger_doubletap";
 
         /**
+         * Whether to enable fingerprint wake-and-unlock.
+         * @hide
+         */
+        public static final String FINGERPRINT_WAKE_UNLOCK = "fingerprint_wake_unlock";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
