6c0e97fc5a6c9974b9a6ec2f148fa0c3be8e8391
vjspranav <pranavasri@live.in>
2022-12-08 22:06:06 +0000

    Revert "base: Double tap to sleep on lockscreen"
    This reverts commit 920ee254d9da5ee1ed2c2c884addbe76e441c730.

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -56,10 +56,8 @@ import android.annotation.Nullable;
 import android.app.Fragment;
 import android.app.StatusBarManager;
 import android.content.ContentResolver;
-import android.content.Context;
-import android.content.res.Configuration;
-import android.content.res.Resources;
 import android.database.ContentObserver;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.Insets;
 import android.graphics.Rect;
@@ -83,7 +81,6 @@ import android.util.IndentingPrintWriter;
 import android.util.Log;
 import android.util.MathUtils;
 import android.view.InputDevice;
-import android.view.GestureDetector;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.VelocityTracker;
@@ -556,8 +553,6 @@ public final class NotificationPanelViewController implements Dumpable {
 
     private final KeyguardIndicationController mKeyguardIndicationController;
 
-    private GestureDetector mDoubleTapGestureListener;
-
     private int mHeadsUpInset;
     private boolean mHeadsUpPinnedMode;
     private boolean mAllowExpandForSmallExpansion;
@@ -939,16 +934,6 @@ public final class NotificationPanelViewController implements Dumpable {
         });
         mBottomAreaShadeAlphaAnimator.setDuration(160);
         mBottomAreaShadeAlphaAnimator.setInterpolator(Interpolators.ALPHA_OUT);
-        mDoubleTapGestureListener = new GestureDetector(mView.getContext(),
-                new GestureDetector.SimpleOnGestureListener() {
-            @Override
-            public boolean onDoubleTap(MotionEvent event) {
-                final PowerManager pm = (PowerManager) mView.getContext().getSystemService(
-                        Context.POWER_SERVICE);
-                pm.goToSleep(event.getEventTime());
-                return true;
-            }
-        });
         mConversationNotificationManager = conversationNotificationManager;
         mAuthController = authController;
         mLockIconViewController = lockIconViewController;
@@ -6165,11 +6150,6 @@ public final class NotificationPanelViewController implements Dumpable {
                         "onTouch: ignore touch, bouncer scrimmed or showing over dream");
                 return false;
             }
-
-            if (mBarState == StatusBarState.KEYGUARD) {
-                mDoubleTapGestureListener.onTouchEvent(event);
-            }
-
 
             // Make sure the next touch won't the blocked after the current ends.
             if (event.getAction() == MotionEvent.ACTION_UP
